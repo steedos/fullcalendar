86fc925438e77be2962e07edb90a3e74484e9f23
yinlianghui <yinlianghui@hotoa.com>
2018-05-14 13:58:00 +0800

    apps #1802 momentjs:moment->steedos:momentjs-moment

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: 'steedos:fullcalendar',
     summary: "Full-sized drag & drop event calendar (jQuery plugin)",
-    version: "3.4.0_1",
+    version: "3.4.0_2",
     git: "https://github.com/steedos/fullcalendar/tree/fix-zoom"
 });
 
 Package.onUse(function(api) {
     api.versionsFrom('METEOR@0.9.2.2');
     api.use([
-        'momentjs:moment@2.8.4',
+        'steedos:momentjs-moment@2.14.1',
         'templating'
     ], 'client');
     api.addFiles([
